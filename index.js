e9ed663910a60e75612cb502e790bac0d3156c2c
Siena Santos <aneis_siena18@yahoo.com>
2018-04-26 14:05:05 +0800

    test

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const emailApi =require ('./api/email')
 const reportingData = require ('./api/reportingData')
 const request = require('request')
 const moment = require('moment')
+var excel = require('excel-stream')
+var fs = require('fs')
 
 const getFirstandLastDay = function(){
   //returns first day and last day of month
@@ -18,51 +20,74 @@ const getFirstandLastDay = function(){
   return { firstDay, lastDay };
 }
 
-const generateReport = function(report){
+const writeFile = function(buffer){
+  // console.log(buffer)
+  fs.writeFile("./file.xls", buffer, 'binary', function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+});
+}
+
+const generateReport = function(report,cb){
   var { url, dateRange } = report
   var { firstDay, lastDay } = getFirstandLastDay()
 
   var link = dateRange ? `${url}?startDate=${firstDay}&endDate${lastDay}` : `${url}?date=${today}`;
   console.log('link ', link)
 
-  request
-    .get(link)
-    .end(function(err, result){ 
-      if(err){
-        console.log(err)
-      }else{
-        console.log('result ', result)
-      }
-    })
+  request.get({
+    url : link,
+  }, function(err, res, body){
+    if(err) console.log(err)
+    else{
+      cb(body)
+      //writeFile(body);
+    }
+  })
 
 }
 
-const job = new CronJob({
-  cronTime: '1 * * * * *',
-  onTick: function() {
 
-    var report = reportingData['booking'];
 
-    var email = 'sienasantos18@gmail.com'
-    var subject = 'CRON JOB'
-    var body = '<p>Cron emailing test.</p>  Time: '+new Date();
-    generateReport(report);
-    // emailApi.sendEmailGeneral(email, subject, body)
-    /*
-     * Runs every day at 700pm
-     */
-    console.log("cron execution - every 1 min ", new Date());
-		// code for your job goes here :)
-  },
-  start: false,
-  timeZone: 'Asia/Manila'
-});
+app.get('/test', function(req,res){
+    generateReport(reportingData['booking'], function(body){
+      fs.writeFile('text.xlsx', body, function(err) {
+        res.sendStatus(err ? 500 : 200);
+      });
 
-job.start();
+      //res.download(body)
+    })
+})
 
-//this does nothing except tell now we're alive
-const port = process.env.PORT || 2345;
-
+// const job = new CronJob({
+//   cronTime: '0 * * * * *',
+//   onTick: function() {
+//
+//     var report = reportingData['booking'];
+//
+//     var email = 'sienasantos18@gmail.com'
+//     var subject = 'CRON JOB'
+//     var body = '<p>Cron emailing test.</p>  Time: '+new Date();
+//     generateReport(report);
+//     // emailApi.sendEmailGeneral(email, subject, body)
+//     /*
+//      * Runs every day at 700pm
+//      */
+//     console.log("cron execution - every min ", new Date());
+// 		// code for your job goes here :)
+//   },
+//   start: false,
+//   timeZone: 'Asia/Manila'
+// });
+//
+// job.start();
+//
+// //this does nothing except tell now we're alive
+ const port = process.env.PORT || 2345;
+//
 app.listen(port, function () {
   console.log('app listening on port: ', port);
 });
